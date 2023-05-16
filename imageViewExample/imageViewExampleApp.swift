5dc5418c7bb75a49ad5203ac5b7421187b6f1b0e
0x67 <evilisnj@gmail.com>
2023-05-17 07:21:03 +0800

    Initial Commit

---
+++ imageViewExample/imageViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  imageViewExampleApp.swift
+//  imageViewExample
+//
+//  Created by 0x67 on 2023-05-17.
+//
+
+import SwiftUI
+
+@main
+struct imageViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
