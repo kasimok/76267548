245883f2783017a68765c25bcf15f3ab3f7490a9
0x67 <evilisnj@gmail.com>
2023-05-17 08:01:24 +0800

    +1

--- imageViewExample/ContentView.swift
+++ imageViewExample/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+      // Convert and create a image from Color Image
+      let image = NSImage(named: "siteLogo")!
+      let imageData = image.tiffRepresentation!
+      let bitmap = NSBitmapImageRep(data: imageData)!
+      let bytes = bitmap.converting(to: .deviceGray, renderingIntent: .default)
+      let pngData = bytes!.representation(using: .png, properties: [NSBitmapImageRep.PropertyKey.compressionFactor: 1.0])!
+      // Now We have grey scaled pngData
+      return ImageView_2(data: pngData)
     }
 }
 
--- imageViewExample/ImageView_2.swift
+++ imageViewExample/ImageView_2.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ImageView_2: View {
+  var data: Data
+  
+  var body: some View {
+    let colorSpace = CGColorSpaceCreateDeviceGray()
+    let cgImageSource = CGImageSourceCreateWithData(data as CFData, nil)!
+    let cgImage = CGImageSourceCreateImageAtIndex(cgImageSource, 0, nil)!
+    let image = NSImage(cgImage: cgImage, size: NSSize(width: cgImage.width, height: cgImage.height))
+    
+    return Image(nsImage: image)
+        .resizable()
+        .aspectRatio(contentMode: .fit)
+  }
+}
+
+extension Data {
+  func castToCPointer<T>() -> T {
+    return self.withUnsafeBytes { $0.pointee }
+  }
+}
+
+
+//struct ImageView_2_Previews: PreviewProvider {
+//  static var previews: some View{
+//    let image = NSImage(named: "siteLogo")!
+//    let imageData = image.tiffRepresentation!
+//    let bitmap = NSBitmapImageRep(data: imageData)!
+//    let bytes = bitmap.bitmapData
+//    let byteBuffer = bytes.bindMemory(to: UInt8.self, capacity: imageData.count)
+//                let byteArray = Array(UnsafeBufferPointer(start: byteBuffer, count: imageData.count))
+//    let byteArray = Array(UnsafeBufferPointer(start: byteBuffer, count: imageData.count))
+//    ImageView_2(data: byteArray)
+//  }
+//}
